a2f368e2410daaf801accd2eeebbce95ff024449
Null <abc612008@126.com>
2015-06-20 08:55:28 +0800

    一些基础的文件

---
+++ HackerMissions/BasicInclude.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+#include "Window\Window.h"
+
+//在这里#include各种文件
---
+++ HackerMissions/HackerMissions.cpp
@@ -0,0 +1,23 @@
+#include "BasicInclude.h"
+
+int main(int argc, char *argv[])
+{
+	auto window = initAndCreateWindow(argc, argv); //初始化窗口
+
+	if (window == nullptr) return -1; //如果加载错误就直接退出程序
+
+	bool running = true;
+
+	while (running) {
+
+		running = !glfwGetKey(window, GLFW_KEY_ESCAPE);  //ESC退出
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	return 0;
+}
